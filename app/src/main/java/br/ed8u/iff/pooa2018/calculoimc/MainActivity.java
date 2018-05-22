247471cad39a1822a83527aed8341cbda7aec5d0
Gabarros <gabarros17@gmail.com>
2018-05-22 15:18:17 -0300

    Correção de Erros

--- app/src/main/java/br/ed8u/iff/pooa2018/calculoimc/MainActivity.java
+++ app/src/main/java/br/ed8u/iff/pooa2018/calculoimc/MainActivity.java
@@ -3,6 +3,7 @@ package br.ed8u.iff.pooa2018.calculoimc;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -39,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
                 calcular();
             }
         });
+
     }
 
 
